f19e9ecca5746389ed74eb26e613925d5f49cece
JISOO LEE <67191178+bazzang@users.noreply.github.com>
2024-05-13 21:23:46 +0900

    first commit

---
+++ helloworld.py
@@ -0,0 +1,49 @@
+#입력을 받고 (디테일 추가)
+
+#default input 은 string type으로 만들어져 있음 
+
+#배열
+# list = [0, 0, 0, 0]
+# list = ["he", "hi", "hu"]
+
+#case 1 : 단순히 정수 일 때,
+#number = int(input())
+
+#case 2 : 수열
+# list1 = list(map(int,input().split()))
+# print(list1)
+
+#case 3 : 단순히 문자 일 때,
+# string =1 input()
+
+#case 4 : 문자열 
+# list2 = list(map(str,input().split()))
+# print(list2) # 안에있는 내용만 출력하고 싶을 때는 print(*list2)
+
+#계산을 하고
+
+
+#출력을 한다
+
+
+# 여러가지 숫자들의 조합, 수열 = list(map(int,input().split()))
+# 여러가지 문자들의 조합, 문자열  = list(map(str,input().split()))
+
+
+# 반복문 
+# for x in range(100): 
+#     print(x)
+
+# number = 0
+# while number < 10 : 
+#     print(number)
+#     number = number + 1
+
+
+# 조건문 
+# name = "지수"
+
+# if name == "이지수" : 
+#     print("이름은 지수야" )
+# else : 
+#     print("이름은 이지수가 아니야")
